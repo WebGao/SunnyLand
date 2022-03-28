3f6354acbe20dc51c18a27f6f091fe18d0753893
WebGao <929817104@qq.com>
2022-03-28 18:31:33 +0800

    add animatin event

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator anim;
     public float speed;
     public float jumpforce;
     // Start is called before the first frame update
@@ -23,14 +24,22 @@ public class PlayerController : MonoBehaviour
     void Movement()
     {
         float horizontalMove = Input.GetAxis("Horizontal"); // 存在小数。-1: Left; 0: Stop; 1: Right.
+        float facedirection = Input.GetAxisRaw("Horizontal"); // 无小数，只有0、1、-1.
+        // 角色行走
         if (horizontalMove != 0)
         {
             rb.velocity = new Vector2(horizontalMove * speed * Time.deltaTime, rb.velocity.y);
+            anim.SetFloat("running", Mathf.Abs(facedirection));
+        }
+        if (facedirection != 0)
+        {
+            transform.localScale = new Vector3(facedirection, 1, 1); // x改变，y、z不改变。
         }
         // 角色跳跃
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+            //anim.SetBool("jumping", true);
         }
     }
 
